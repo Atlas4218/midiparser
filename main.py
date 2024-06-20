efaf5941e9f8586b39aff15142ce236e95c869b9
Roman ASTIE <roman.astie@wanadoo.fr>
2024-06-20 15:48:21 +0200

    commit

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from mido import MidiFile, MidiTrack, Message, MetaMessage
 
-file_name = input("File path ?")
+file_name = ""
 mid_file = MidiFile(file_name)
 result_file = open("result.txt", 'w')
 tick_per_beat = mid_file.ticks_per_beat
 tempo = 0
-time = 0
 for i, track in enumerate(mid_file.tracks):
+    time = 0
     print('Track {}: {}'.format(i, track.name))
+    result_file.write("Type\tTime\tValue\n")
     for msg in track:
         time += msg.time
         if isinstance(msg, MetaMessage):
             if msg.type == 'set_tempo':
                 tempo = msg.tempo
             if (msg.type == 'end_of_track'):
-                result_file.write(msg.type + "\t" + str((time * tempo) / tick_per_beat / 1000000) + "\n")    
+                result_file.write('{}\t{}\n'.format(msg.type, (time * tempo) / tick_per_beat / 1000000))  
         if (msg.type == 'note_on'):
             if (msg.velocity > 0):
-                result_file.write(msg.type + "\t" + "\t" + str((time * tempo) / tick_per_beat / 1000000) + str(msg.note) + "\n")
+                result_file.write('{}\t{}\t{}\n'.format(msg.type, (time * tempo) / tick_per_beat / 1000000, msg.note))  
         print(str(msg))
